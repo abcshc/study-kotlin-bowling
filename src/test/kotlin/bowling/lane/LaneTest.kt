a15088ed24d61ea2638cec67c31509fbd75abfa6
소희철 <thrrkthrrk@gmail.com>
2019-04-11 23:37:04 +0900

    Add incomplete kotlin test

---
+++ src/test/kotlin/bowling/lane/LaneTest.kt
@@ -0,0 +1,23 @@
+package bowling.lane
+
+import org.junit.Before
+import kotlin.test.Test
+
+class LaneTest {
+    @Before
+    fun setup() {
+
+    }
+
+    @Test
+    fun test_knockedOver() {
+    }
+
+    @Test
+    fun test_getKnockedOverCount(): Int {
+    }
+
+    @Test
+    fun test_initializeBowlingPins() {
+    }
+}
