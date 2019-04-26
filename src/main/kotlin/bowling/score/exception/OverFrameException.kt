de7298d4cb7ab2a434610c8cc54aad8d9c3977cf
소희철 <thrrkthrrk@gmail.com>
2019-04-26 22:00:27 +0900

    Add raised exception case in ScoreBoardTest

---
+++ src/main/kotlin/bowling/score/exception/OverFrameException.kt
@@ -0,0 +1,5 @@
+package bowling.score.exception
+
+import java.lang.RuntimeException
+
+class OverFrameException : RuntimeException()
--- src/test/kotlin/bowling/score/ScoreBoardTest.kt
+++ src/test/kotlin/bowling/score/ScoreBoardTest.kt
@@ -1,8 +1,11 @@
 package bowling.score
 
+import bowling.score.exception.InvalidRollCountException
+import bowling.score.exception.OverFrameException
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class ScoreBoardTest {
     private lateinit var scoreBoard: ScoreBoard
@@ -10,6 +13,16 @@ class ScoreBoardTest {
     @BeforeTest
     fun setup() {
         scoreBoard = ScoreBoard()
+    }
+
+    @Test
+    fun `게임 시작 전 상태 확인`() {
+        assertEquals(GameStatus.READY, scoreBoard.getGameStatus())
+    }
+
+    @Test
+    fun `게임 시작 후 상태 확인`() {
+        assertEquals(GameStatus.PLAYING, scoreBoard.getGameStatus())
     }
 
     @Test
@@ -87,14 +100,92 @@ class ScoreBoardTest {
         assertEquals(GameStatus.COMPLETED, scoreBoard.getGameStatus())
     }
 
-    // TODO: extension이 되어야 하는 함수로 보임 수정하기
-    private fun ScoreBoard.repeatableRoll2TimesWith3Points(repeat: Int): ScoreBoard {
+    @Test
+    fun `마지막 프레임 추가 점수입력`() {
+        scoreBoard.repeatableRoll2TimesWith3Points(9)
+        scoreBoard.roll(5)
+        scoreBoard.roll(5)
+        scoreBoard.roll(5)
+
+
+        assertEquals(10, scoreBoard.getCurrentFrameNumber())
+        assertEquals(75, scoreBoard.getTotalScore())
+        assertEquals(GameStatus.COMPLETED, scoreBoard.getGameStatus())
+    }
+
+    @Test(expected = InvalidRollCountException::class)
+    fun `10점보다 많은 점수입력`() {
+        scoreBoard.roll(11)
+    }
+
+    @Test
+    fun `6프레임에서 10점보다 많은 점수입력`() {
+        scoreBoard.repeatableRoll2TimesWith3Points(5)
+
+        assertEquals(6, scoreBoard.getCurrentFrameNumber())
+        assertEquals(30, scoreBoard.getTotalScore())
+        assertFailsWith(InvalidRollCountException::class) {
+            scoreBoard.roll(11)
+        }
+    }
+
+    @Test(expected = InvalidRollCountException::class)
+    fun `10점보다 많은 점수입력(2번 입력의 합)`() {
+        scoreBoard.roll(5)
+        scoreBoard.roll(6)
+    }
+
+    @Test
+    fun `마지막 프레임 이후 점수입력`() {
+        scoreBoard.repeatableRoll2TimesWith3Points(10)
+
+        assertEquals(10, scoreBoard.getCurrentFrameNumber())
+        assertEquals(60, scoreBoard.getTotalScore())
+        assertEquals(GameStatus.COMPLETED, scoreBoard.getGameStatus())
+
+        assertFailsWith(OverFrameException::class) {
+            scoreBoard.roll(5)
+        }
+    }
+
+    @Test
+    fun `마지막 프레임 이후 점수입력 스트라이크 포함`() {
+        scoreBoard.repeatableRoll2TimesWith3Points(8)
+        scoreBoard.roll(10)
+        scoreBoard.roll(10)
+        scoreBoard.roll(10)
+        scoreBoard.roll(10)
+
+        assertEquals(10, scoreBoard.getCurrentFrameNumber())
+        assertEquals(108, scoreBoard.getTotalScore())
+        assertEquals(GameStatus.COMPLETED, scoreBoard.getGameStatus())
+
+        assertFailsWith(OverFrameException::class) {
+            scoreBoard.roll(5)
+        }
+    }
+
+    @Test
+    fun `마지막 프레임 추가 점수입력 후 잘못된 점수입력`() {
+        scoreBoard.repeatableRoll2TimesWith3Points(9)
+        scoreBoard.roll(5)
+        scoreBoard.roll(5)
+        scoreBoard.roll(5)
+
+
+        assertEquals(10, scoreBoard.getCurrentFrameNumber())
+        assertEquals(75, scoreBoard.getTotalScore())
+        assertEquals(GameStatus.COMPLETED, scoreBoard.getGameStatus())
+
+        assertFailsWith(OverFrameException::class) {
+            scoreBoard.roll(5)
+        }
+    }
+
+    private fun ScoreBoard.repeatableRoll2TimesWith3Points(repeat: Int) {
         for (i in 0..repeat) {
             this.roll(3)
             this.roll(3)
         }
-        return this
     }
-    // TODO: 잘못된 점수 입력 시도 시 예외 발생
-    // TODO: 스코어 보드가 각종 보너스 점수를 계산하여 최종점수를 출력할 수 있어야 함
 }
