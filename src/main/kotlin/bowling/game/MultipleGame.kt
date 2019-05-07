ee8503798e30759738efca694dcae578f00132ba
소희철 <thrrkthrrk@gmail.com>
2019-05-07 23:20:00 +0900

    Add MultipleGame lane, player

--- src/main/kotlin/bowling/game/MultipleGame.kt
+++ src/main/kotlin/bowling/game/MultipleGame.kt
@@ -3,7 +3,7 @@ package bowling.game
 // 멀티플레이 고려사항
 // 1레인 플레이, 2레인 플레이
 // 게임 플레이 방식에 따라 순서 정하기
-class MultipleGame : Game {
+class MultipleGame(val numberOfLane: Int, val numberOfPlayer: Int) : Game {
     override fun roll(score: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
---
+++ src/test/kotlin/bowling/game/MultipleGameTest.kt
@@ -0,0 +1,15 @@
+package bowling.game
+
+import org.junit.Test
+
+class MultipleGameTest {
+    @Test
+    fun `2명 플레이어 게임 진행 1레인 게임진행`() {
+        val game = MultipleGame(1, 2)
+        game.roll(3)
+        game.roll()
+    }
+    @Test
+    fun `2명 플레이어 게임 진행 2레인 게임진행`() {
+    }
+}
