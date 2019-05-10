ed7f769fbef4726e36cf28448c5f015f4028d849
소희철 <thrrkthrrk@gmail.com>
2019-05-10 23:22:31 +0900

    Add ScoreBoard in MultipleGame

--- src/main/kotlin/bowling/game/MultipleGame.kt
+++ src/main/kotlin/bowling/game/MultipleGame.kt
@@ -1,6 +1,7 @@
 package bowling.game
 
 import bowling.lane.Lane
+import bowling.score.ScoreBoard
 
 // 멀티플레이 고려사항
 // 1레인 플레이, 2레인 플레이
@@ -11,6 +12,12 @@ import bowling.lane.Lane
 // 1. 레인이 필요함
 // 2. 플레이어를 배분할 게임이 필요함.
 // 3. 플레이어가 정해지면 레인을 배분함.
-class MultipleGame(val numberOfLane: Int, val numberOfPlayer: Int) {
+// 4. 플레이어 수만큼 스코어 보드 생성
+class MultipleGame(numberOfLane: Int, val numberOfPlayer: Int) {
     val laneList: List<Lane> = if (numberOfLane == 1) listOf(Lane()) else listOf(Lane(), Lane())
+    val scoreBoardList = ArrayList<ScoreBoard>()
+    init {
+        for (i in 0..numberOfPlayer)
+            scoreBoardList.add(ScoreBoard())
+    }
 }
